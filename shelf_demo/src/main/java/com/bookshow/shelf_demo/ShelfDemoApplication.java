8cdf9167d57a0ec2f28d5cdcc7b03e8c47a710d1
student <rod.qasolutions@gmail.com>
2021-03-30 03:30:45 +0000

    spring boot maven

---
+++ shelf_demo/src/main/java/com/bookshow/shelf_demo/ShelfDemoApplication.java
@@ -0,0 +1,13 @@
+package com.bookshow.shelf_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShelfDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShelfDemoApplication.class, args);
+	}
+
+}
---
+++ shelf_demo/src/test/java/com/bookshow/shelf_demo/ShelfDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookshow.shelf_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShelfDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
